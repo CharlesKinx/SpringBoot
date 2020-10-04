a84a5ed422896ed05722ef97d1a68866a6609d82
CharlesKinx <664410795@qq.com>
2020-10-04 22:23:42 +0800

    完成提交问题BUG

--- src/main/java/com/example/demo/controller/PublishController.java
+++ src/main/java/com/example/demo/controller/PublishController.java
@@ -36,6 +36,9 @@ public class PublishController {
             HttpServletRequest request,
             Model model
     ){
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
 
         if (title == null || title == "") {
             model.addAttribute("error","标题不能为空！");
@@ -49,9 +52,7 @@ public class PublishController {
             model.addAttribute("error","标签不能为空！");
             return "publish";
         }
-        model.addAttribute("title",title);
-        model.addAttribute("description",description);
-        model.addAttribute("tag",tag);
+
         User user = null;
         Cookie[]  cookies= request.getCookies();
         for(Cookie cookie:cookies){
