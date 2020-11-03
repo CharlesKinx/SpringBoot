3b4b6c3a117aa902c4acd5f1549dd3d670fd8f27
CharlesKinx <664410795@qq.com>
2020-11-03 18:52:55 +0800

    提交信息

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -1,13 +1,30 @@
 package com.example.demo.controller;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProfileController {
-    @GetMapping("/profile/{action}")
 
-    public String profile(){
+    @GetMapping("/profile/{action}")
+    public String profile(
+            @PathVariable(name = "action") String action,
+            Model model
+    ) {
+
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName","我的提问");
+
+        }else if("replies".equals(action)){
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName","我的回复");
+        }
         return "profile";
     }
+
+
 }
