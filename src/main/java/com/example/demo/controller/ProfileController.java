2e2e60905fbebd42d58d510dcfa8bc8fe693afe4
CharlesKinx <664410795@qq.com>
2020-11-06 21:15:06 +0800

    修复代码复用问题

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -19,9 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 public class ProfileController {
 
     @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
     private QuestionService questionService;
 
     @GetMapping("/profile/{action}")
@@ -32,21 +29,8 @@ public class ProfileController {
             @RequestParam(name = "page", defaultValue = "1") Integer pages,
             @RequestParam(name = "size", defaultValue = "5") Integer size
     ) {
-        User user=null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user= userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
 
+        User user = (User)request.getSession().getAttribute("user");
         if(user==null){
             return "redirect:/";
         }
